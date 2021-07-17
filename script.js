d1a52464496ef23c7091f7ab2ecbd01b990d67e1
Anurag Harsh <anuragharsh626@gmail.com>
2021-07-17 18:44:52 +0530

    Add auto fill for skill bars in skills section

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
+/*Add Auto Fill to skill bars of the skills section*/
+
+function initialiseBar(bar){
+    bar.style.width=0+'%';
+}
+
+function fillBar(bar){
+    let targetWidth=bar.getAttribute('data-bar-width');
+    let currentWidth=0;
+    let interval=setInterval(function(){
+       if(currentWidth>=targetWidth){
+       clearInterval(interval);
+       }
+       currentWidth++;
+       bar.style.width=currentWidth+'%'
+    },5);  
+}
+
+var animationDone=[false,false,false,false,false,false];
+function checkScroll(){
+    var progressBars=document.querySelectorAll('.skill-progress > div');
+    let count=0;
+    for(let bar of progressBars){
+        let coordinates=bar.getBoundingClientRect();
+        if(!animationDone[count]&&coordinates.top<=window.innerHeight-coordinates.height){
+            initialiseBar(bar);
+            fillBar(bar);
+            animationDone[count]=true;
+        }
+        else if(animationDone[count]==true&&coordinates.top>window.innerHeight){
+            initialiseBar(bar);
+            animationDone[count]=false;
+        }
+        ++count;
+    }
+}
+
+window.addEventListener('scroll',checkScroll);
+
+/*Add Smooth Scroll to navigation section of header*/
+
+
 var navMenuAnchorTags=document.querySelectorAll('#header_navigation a');
 var responsivenavMenuAnchorTags=document.querySelectorAll('.responsive-navigation a');
 var interval;
